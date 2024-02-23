612f55d006ed9dd74d073c62ef5376919b74b45b
francisgan <102556156+francisgan@users.noreply.github.com>
2024-02-22 19:05:23 -0800

    Update LSTM_simulate.py

--- LSTM_simulate.py
+++ LSTM_simulate.py
@@ -10,41 +10,67 @@ import pickle
 import model
 import configs
 from torch.profiler import profile, record_function, ProfilerActivity
+import argparse
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-model = model.LSTMClassifier(52, 250, 25, 2).to(device)
-model.load_state_dict(torch.load("./models/LSTM.pth")) 
-with open ('small_sample', 'rb') as fp:
-    data_feature = pickle.load(fp)
-fp.close()
-model.eval()
 
-with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], profile_memory=True, record_shapes=True, with_stack=True) as prof:
-    with record_function("model_inference"):
-        #################### Profiling this part##########################
+def main(args):
+    device = torch.device("cpu")
+    if args.device == "gpu":
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("GPU availabel: {}, Current device: {}".format(torch.cuda.is_available(), device))
         
+    LSTM = model.LSTMClassifier(52, 250, 25, 2).to(device)
+    if torch.cuda.is_available() and args.device == "gpu":
+        LSTM.load_state_dict(torch.load("./models/LSTM.pth")) 
+    else:
+        LSTM.load_state_dict(torch.load("./models/LSTM.pth",map_location=torch.device('cpu'))) 
 
-        input = torch.Tensor([data_feature[0]]).to(device)
-        output = model(input)
+    with open ('small_sample', 'rb') as fp:
+        data_feature = pickle.load(fp)
+    fp.close()
+    LSTM.eval()
 
-        pred_index = torch.max(output, 1)[1]
+    act = [ProfilerActivity.CPU]
+    if args.device == "gpu" and torch.cuda.is_available():
+        act.append(ProfilerActivity.CUDA)
 
-        pred = torch.max(output, 1)[1]
-        print("Activity ID: ", pred.item())
-        ##################################################################
+    with profile(activities=act, profile_memory=True, record_shapes=True, with_stack=True) as prof:
+        with record_function("model_inference"):
+            #################### Profiling this part##########################
+            
 
-txt = prof.key_averages().table(sort_by="self_cpu_memory_usage")
-path = "./profiling/LSTM/table_"+ configs.environment_name +".txt"
-text_file = open(path, "w")
-text_file.write(txt)
-text_file.close()
+            input = torch.Tensor([data_feature[0]]).to(device)
+            output = LSTM(input)
 
-path = "./profiling/LSTM/chromeTrace_"+ configs.environment_name +".json"
-prof.export_chrome_trace(path)
+            pred_index = torch.max(output, 1)[1]
 
-path = "./profiling/LSTM/profiler_stacks_cuda_"+ configs.environment_name +".txt"
-prof.export_stacks(path, "self_cuda_time_total")
+            pred = torch.max(output, 1)[1]
+            print("Activity ID: ", pred.item())
+            ##################################################################
 
-path = "./profiling/LSTM/profiler_stacks_cpu_"+ configs.environment_name +".txt"
-prof.export_stacks(path, "self_cpu_time_total")
+    environment_name = args.env
+
+    txt = prof.key_averages().table(sort_by="self_cpu_memory_usage")
+    path = "./profiling/LSTM/table_"+ environment_name +".txt"
+    text_file = open(path, "w")
+    text_file.write(txt)
+    text_file.close()
+
+    path = "./profiling/LSTM/chromeTrace_"+ environment_name +".json"
+    prof.export_chrome_trace(path)
+
+    if args.device == "gpu" and torch.cuda.is_available():
+        path = "./profiling/LSTM/profiler_stacks_cuda_"+ environment_name +".txt"
+        prof.export_stacks(path, "self_cuda_time_total")
+
+    path = "./profiling/LSTM/profiler_stacks_cpu_"+ environment_name +".txt"
+    prof.export_stacks(path, "self_cpu_time_total")
+
+if __name__ == "__main__":
+    #Parse the input arguments
+    print(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str)
+    parser.add_argument('--device', type=str)
+    args = parser.parse_args()
+    main(args)
